eba4fc15d208ba2e6e1fd4625c2f9e76aaf809f5
Lindq <lindq@x16.com>
2015-08-25 15:39:42 +0800

    消息

--- public/javascripts/app/directive/recorder_left_pic.js
+++ public/javascripts/app/directive/recorder_left_pic.js
@@ -11,7 +11,7 @@
  * Factory in the elmApp.
  */
 angular.module('fscApp')
-    .directive('recorderLeftPic', function(global) {
+    .directive('recorderLeftPic', function(global,dialog) {
         return {
             restrict: 'E',
             templateUrl: '/node_static/javascripts/app/view/directive/recorder_left_pic.html',
@@ -21,6 +21,21 @@ angular.module('fscApp')
             },
             link: function (scope, element, attrs) {
                 scope.resUrl = global.cache.resUrl;
+                scope.openPhoto=function(message)
+                {
+                    dialog.complexBox(
+                        {
+                            templateUrl: '/node_static/javascripts/app/view/menus/photo-menu.html',
+                            size:'md',
+                            onComplete: function (dialogScope,modalInstance) {
+                                dialogScope.message = message;
+                                dialogScope.resUrl = scope.resUrl;
+                                dialogScope.closePhoto = function(){
+                                    modalInstance.close();
+                                }
+                            }
+                        })
+                };
             }
         }
     })
--- public/javascripts/app/directive/recorder_right_pic.js
+++ public/javascripts/app/directive/recorder_right_pic.js
@@ -7,7 +7,7 @@
  * Factory in the elmApp.
  */
 angular.module('fscApp')
-    .directive('recorderRightPic', function(global){
+    .directive('recorderRightPic', function(global,dialog){
         return {
             restrict: 'E',
             templateUrl: '/node_static/javascripts/app/view/directive/recorder_right_pic.html',
@@ -17,6 +17,22 @@ angular.module('fscApp')
             },
             link: function(scope, element, attrs,SessionCtrl){
                 scope.resUrl = global.cache.resUrl;
+                scope.openPhoto=function(message)
+                {
+                    dialog.complexBox(
+                        {
+                            templateUrl: '/node_static/javascripts/app/view/menus/photo-menu.html',
+                            size:'md',
+                            onComplete: function (dialogScope,modalInstance) {
+                                dialogScope.message = message;
+                                dialogScope.resUrl = scope.resUrl;
+                                dialogScope.closePhoto = function(){
+                                    modalInstance.close();
+                                }
+                            }
+                        })
+                };
+
             }
         }
     })
