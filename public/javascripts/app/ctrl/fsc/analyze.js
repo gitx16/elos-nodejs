a11bbd7296130c17caa0846acd1c8ea307262dd4
pengfei <270137794@qq.com>
2015-08-26 14:01:29 +0800

    手机端教学计划

---
+++ public/javascripts/app/ctrl/fsc/analyze.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name fscApp.controller:AnalyzeCtrl
+ * @description
+ * # AnalyzeCtrl
+ * Controller of the fscApp
+ */
+angular.module('fscApp')
+    .controller('AnalyzeCtrl', function ($scope,$routeParams,resourcePool) {
+    });
--- routes/app.js
+++ routes/app.js
@@ -13,4 +13,7 @@ router.get('/plan_nodes/:id', function (req, res) {
 router.get('/work_view/:id', function (req, res) {
     res.render('app/work_view', { workId: req.params.id });
 });
+router.get('/analyze/:id', function (req, res) {
+    res.render('app/work_view', { workId: req.params.id });
+});
 module.exports = router;
